57d54cfc0a7469fb57eae103f145c47f8d5554a1
tainosu <sekikawa0916@gmail.com>
2024-12-10 00:28:14 +0900

    デプロイ

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/my-portfolio",
   output: "export",
+  images: { unoptimized: true } ,
 };
 
 export default nextConfig;
