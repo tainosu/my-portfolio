83a9a0d340fc6ce85621f2bc99d8dfe52bd02c33
tainosu <sekikawa0916@gmail.com>
2024-12-10 00:41:02 +0900

    framer-motionの依存関係を修正

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/my-portfolio",
+  // basePath: "/my-portfolio",
   output: "export",
-  images: { unoptimized: true } ,
+  // images: { unoptimized: true } ,
 };
 
 export default nextConfig;
